ab01fce9fe86618402a5d645d71442f85fa92769
Utkarsh Nagar <utkarshnagar2000@gmail.com>
2024-06-24 10:43:40 +0000

    feat: optimised with DP

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.Scanner;
 
 class Diffy{
-    private static String diffString = "";
+    private static int[][] dp;
 
     private static String readContent(String path) throws Exception{
         return new String(Files.readAllBytes(Paths.get(path)));
@@ -14,44 +14,44 @@ class Diffy{
 
     // move both i and j ptrs if characters match else
     // move 1 pointer at a time
-    private static String find(String a,String b,int i,int j,String ans,int[][] dp){
-        if(i<0 || j<0)return ans;
-        if(dp[i][j]!=-1)return ans;
-
-        if(a.charAt(i) == b.charAt(j)){
-            ans += a.charAt(i);
-            if(ans.length() > diffString.length())diffString = ans;
-            dp[i][j] = Math.max(ans.length(),diffString.length());
-            return find(a,b,i-1,j-1,ans,dp);
+    private static String findLCSHelper(String a,String b,int i,int j){
+        if (i < 0 || j < 0) {
+            return "";
         }
 
-        String s1 = find(a,b,i-1,j,ans,dp);
-        String s2 = find(a,b,i,j-1,ans,dp);
-        dp[i][j] = Math.max(s1.length(),s2.length());
-        if(s1.length() > s2.length()){
-            diffString = s1;
-            return s1;
+        if (dp[i][j] != -1) {
+            return "";
         }
-        diffString = s2;
-        return s2;
+
+        if (a.charAt(i) == b.charAt(j)) {
+            String result = findLCSHelper(a, b, i - 1, j - 1) + a.charAt(i);
+            dp[i][j] = result.length();
+            return result;
+        } 
+        String lcs1 = findLCSHelper(a, b, i - 1, j);
+        String lcs2 = findLCSHelper(a, b, i, j - 1);
+        String result = (lcs1.length() > lcs2.length()) ? lcs1 : lcs2;
+        dp[i][j] = result.length();
+        return result;
     }
 
     private static void LCSCompare(String content1,String content2){
-        int[][] dp = new int[content1.length()+10][content2.length()+10];
-        for(int[] v:dp){
-            Arrays.fill(v,-1);
+        int m = content1.length();
+        int n = content2.length();
+        dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
         }
-        find(content1,content2,content1.length()-1,content2.length()-1,"",dp);
+        System.out.println(findLCSHelper(content1,content2,content1.length()-1,content2.length()-1));
     }
 
     public static void main(String[] args) throws Exception{
         // String content1 = readContent(args[1]);
         // String content2 = readContent(args[2]);
         String content1 = "ABCDEF";
-        String content2 = "CDGFEF";
+        String content2 = "CsdfdfDEF";
         LCSCompare(content1,content2);
-        StringBuilder se = new StringBuilder(diffString);
-         
-        System.out.println("Difference: "+se.reverse());        
     }
 }
